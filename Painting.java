037b5f2e5ab01d570756c1f97154e5feaaa6650e
AnnZombia <drugaleva@mail.ru>
2015-05-27 17:57:02 +0300

    27.05

--- Painting.java
+++ Painting.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,58 +10,154 @@ import javax.swing.JPanel;
 
 
 public class Painting extends JFrame {
-	mousePoint click = new mousePoint(this);
 	Panel Panel = new Panel();
 
 	
 	Painting() {
 		super();
-		setSize(300, 200);
+		setSize(600, 400);
 		setLocation (300, 200);
-		setContentPane(Panel);
-		setVisible(true); }
+		setDefaultCloseOperation (EXIT_ON_CLOSE); }
 	
 	public static void main(String[] args) {
-		Painting MyPaint = new Painting(); }
-	
-	} 
+		Painting MyPaint = new Painting();
+		Panel Panel = new Panel();
+		mousePoint mousePoint = new mousePoint();
+		MyPaint.setContentPane(Panel);
+		MyPaint.setVisible(true); }	} 
 	
 
-class Panel extends JPanel {
+class Panel extends JPanel implements Runnable {
 	mousePoint clickIn = new mousePoint(this);
-	boolean Flag;
+	volatile boolean Flag = false;
+	volatile boolean Pressed = false;
+	int k=0; int flagLAST; int miniK;
+	int OldX=0; int OldY=0;	int miniFlag=3;
 	
 	Panel() {
 		super();
+		Thread stream = new Thread(this);
+	   	stream.start();
 		setBackground(Color.RED);
-		addMouseListener(clickIn);  } 
+		addMouseListener(clickIn); 
+		addMouseMotionListener(clickIn); } 
 	
 	public void paintComponent(Graphics g) {
-		if (Flag) 
-			{g.fillRect(clickIn.clickPoint.x, clickIn.clickPoint.y, 30, 30);
-			System.out.println("4tooo");
-			Flag=false;
-			System.out.println("4tooo");}
-	} 
+		if (Flag) {
+			module();
+			System.out.println("k="+k);
+			System.out.println("k="+k);
+			while ( OldX != clickIn.x && OldY != clickIn.y) {
+					flagLAST=0;
+					System.out.println("FLAG="+miniFlag);
+					for (k=miniK; k > 0; k--) {
+						Calculate();
+						flagLAST=1;
+						g.fillRect(OldX-2, OldY-2, 4, 4); 
+						System.out.println("OLDX="+OldX+" OLDY="+OldY); } }
+			OldX=clickIn.x;
+			OldY=clickIn.y;
+			g.fillRect(clickIn.x-2, clickIn.y-2, 4, 4); } }
+
+	void Calculate() {
+		if (miniFlag == 1) {
+			if (OldX < clickIn.x ) {
+				if (flagLAST == 0) {
+					if (OldY < clickIn.y )
+						OldY++; 
+					if (OldY > clickIn.y )
+						OldY--; }
+				OldX++; }
+			if (OldX > clickIn.x) {
+				if (flagLAST == 0) {
+					if (OldY < clickIn.y )
+						OldY++; 
+					if (OldY > clickIn.y )
+						OldY--; }
+				OldX--; } }
+		
+		if (miniFlag == 2) {
+			if (OldY < clickIn.y ) {
+				if (flagLAST == 0) {
+					if (OldX < clickIn.x )
+						OldX++; 
+					if (OldX > clickIn.x )
+						OldX--; }
+				OldY++; }
+			if (OldY > clickIn.y) {
+				if (flagLAST == 0) {
+					if (OldX < clickIn.x )
+						OldX++; 
+					if (OldX > clickIn.x )
+						OldX--; }
+				OldY--; } }
+		
+		if (miniFlag == 3) {
+			if (OldY < clickIn.y )
+				OldY++; 
+			if (OldY > clickIn.y )
+				OldY--;
+			if (OldX < clickIn.x ) 
+				OldX++; 
+			if (OldX > clickIn.x )
+				OldX--; }
 	
-}
+		if (miniFlag == 4) {
+			if (OldX < clickIn.x ) 
+				OldX++; 
+			if (OldX > clickIn.x )
+				OldX--; } 
 
+		if (miniFlag == 4) {
+			if (OldY < clickIn.y ) 
+				OldY++; 
+			if (OldY > clickIn.y )
+				OldY--; } }
+	
+	void module() {
+		int Dy= OldY - clickIn.y;
+		int Dx= OldX - clickIn.x;
+		if (Dx < 0)
+			Dx=-Dx;
+		if (Dy < 0)
+			Dy=-Dy;
+		if (Dy != 0 && Dx != 0) {
+			if (Dx/Dy != 0) {
+					k=Dx/Dy;
+					miniFlag=1; }
+			else 	{k=Dy/Dx; 
+					miniFlag=2; } }	
+		if (Dy == Dx && Dy !=0) {
+			miniFlag=3; 
+			k=Dy; }
+		if (Dy == 0) {
+			k = Dx;
+			miniFlag=4; }
+		if (Dx == 0) {
+			k = Dy;
+			miniFlag=5; }
+		miniK=k;}
+	
+	@Override
+	public void run() {	} }
 
-class mousePoint implements MouseListener {
-	clickedPoint clickPoint;
-	Painting copy;
-	Panel copy1;
+class mousePoint implements MouseListener, Runnable, MouseMotionListener {
+	Panel Screen;
+	int x=0; int y=0;
 	
 	mousePoint(Panel copy) {
-		this.copy1=copy;	}
+		this.Screen=copy;	}
 	
-	mousePoint(Painting copy) {
-		this.copy=copy;	}
+	mousePoint() {
+	Thread stream = new Thread(this);
+   	stream.start(); }
 	
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
-		clickPoint=new clickedPoint(arg0.getX(),arg0.getY()); 
-		copy1.Flag = true; }
+		clickedPoint(arg0.getX(),arg0.getY()); 
+		Screen.Flag = true;
+		Screen.repaint();
+		Screen.Flag = false; }
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {	}
@@ -70,15 +167,33 @@ class mousePoint implements MouseListener {
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		clickPoint=new clickedPoint(arg0.getX(),arg0.getY());
-		copy1.Flag = true; }
+		clickedPoint(arg0.getX(),arg0.getY());
+		Screen.OldX=x;
+		Screen.OldY=y;
+		Screen.Flag = true;
+		Screen.Pressed = true;
+		Screen.repaint(); }
 
 	@Override
-	public void mouseReleased(MouseEvent arg0) { } }
+	public void mouseReleased(MouseEvent arg0) {
+		clickedPoint(arg0.getX(),arg0.getY());
+		Screen.Flag = false;
+		Screen.Pressed = false;
+		Screen.repaint(); } 
+	
+	void clickedPoint (int x, int y) {
+		this.x=x;
+		this.y=y; }
 
-class clickedPoint {
-		int x; int y;
-		
-	clickedPoint (int e, int r) {
-		x=e; y=r; } }
+	@Override
+	public void run() {	}
+	
+	@Override
+	public void mouseDragged(MouseEvent arg0) {		
+		if (Screen.Pressed)
+		{clickedPoint(arg0.getX(),arg0.getY());
+		Screen.repaint();}	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) { } }
 	
