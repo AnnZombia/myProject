b00a32ff5337f0ffea923055467cee180ecd207f
AnnZombia <drugaleva@mail.ru>
2015-06-03 16:57:23 +0300

    03.06

--- Painting.java
+++ Painting.java
@@ -1,6 +1,8 @@
 import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
@@ -11,75 +13,78 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Painting extends JFrame {
-	Panel Panel = new Panel();
+	int width = 700; int height = 500; 
+	Color COLOR=new Color(221,125,75);
+	MainPanel MainPan = new MainPanel(width, height, COLOR);
+	Button Button=new Button();
+	ButtonAction ButAction=new ButtonAction(MainPan);
 
-	
 	Painting() {
 		super();
-		setBackground(Color.PINK);
-		setSize(700, 500);
+		setTitle("Paint");
+		setLayout(null);
+		getContentPane().setBackground(COLOR);
+		setSize(width, height);
 		setLocation (220, 180);
-		setResizable(false);
-		setDefaultCloseOperation (EXIT_ON_CLOSE); }
+		setResizable(true);
+		setDefaultCloseOperation (EXIT_ON_CLOSE);
+		Button.addActionListener(ButAction);
+		add(Button);
+		add(MainPan);
+		setVisible(true); }
 	
 	public static void main(String[] args) {
-		Painting MyPaint = new Painting();
-		Panel Panel = new Panel();
-		MyPaint.setContentPane(Panel);
-		MyPaint.setVisible(true); }	} 
+		Painting MyPaint = new Painting(); }	}
 
-class Panel extends JPanel {
-	JButton Button=new JButton();
-	ButtonAction ButAction=new ButtonAction(this);
+
+
+class MainPanel extends JPanel {
 	mousePoint clickIn = new mousePoint(this);
-	volatile boolean Flag = false;
-	volatile boolean Pressed = false;
-	int OldX; int OldY;
+	boolean Pressed = false;
+	int OldX=0; int OldY=0;
+	boolean Flag = false;
+	int width; int height; Color COLOR;
 	
-	Panel() {
+	MainPanel(int w, int h, Color COL) {
 		super();
-		Button.setText("Œ◊»—“»“‹");
-		setLayout(new BorderLayout());
-		addMouseListener(clickIn); 
-		Button.addActionListener(ButAction);
-		addMouseMotionListener(clickIn);
-		add("North", Button); } 
-	
-	public void clear() {
-		Graphics g=this.getGraphics();
-		clearING(g); }
-	
-	public void clearING(Graphics g) {
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setColor(Color.PINK); 
-		g2.fillRect(0, 0, 700, 500); }
+		width = w-80;
+		height = h;
+		COLOR = COL;
+		setSize(w, h);
+		setLayout(null);
+		setLocation (80, 0);
+ 		addMouseListener(clickIn); 
+		addMouseMotionListener(clickIn); }
 	
 	public void paintComponent(Graphics g) {
-			if (Flag) {
-				Graphics2D g2 = (Graphics2D) g;
-				g2.setStroke(new BasicStroke(4.0f)); 
-				g2.drawLine(OldX, OldY, clickIn.x, clickIn.y);
-				OldX=clickIn.x;
-				OldY=clickIn.y;
-				g2.fillOval(clickIn.x-2, clickIn.y-2, 4, 4); } } }
 
-	class mousePoint implements MouseListener, MouseMotionListener {
-		Panel Screen;
+			Graphics2D g2 = (Graphics2D) g;
+			
+		if (Pressed) {
+			g2.setStroke(new BasicStroke(4.0f)); 
+			g2.drawLine(OldX, OldY, clickIn.x, clickIn.y);
+			OldX=clickIn.x;
+			OldY=clickIn.y;
+			g2.fillOval(clickIn.x-2, clickIn.y-2, 4, 4); }
+		if (Flag) {
+			g2.setColor(COLOR);
+			g2.fillRect(0, 0, this.getWidth(), this.getHeight());
+			Flag=false; } } }
+
+class mousePoint implements MouseListener, MouseMotionListener {
+	MainPanel Screen;
 		int x=0; int y=0;
 	
-		mousePoint(Panel copy) {
-			this.Screen=copy;	}
+		mousePoint(MainPanel copy) {
+			Screen=copy;	}
 	
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		clickedPoint(arg0.getX(),arg0.getY()); 
-		Screen.Flag = true;
-		Screen.repaint();
-		Screen.Flag = false; }
+	public void mouseClicked(MouseEvent arg0) { }
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {	}
@@ -92,14 +97,12 @@ class Panel extends JPanel {
 		clickedPoint(arg0.getX(),arg0.getY());
 		Screen.OldX=x;
 		Screen.OldY=y;
-		Screen.Flag = true;
 		Screen.Pressed = true;
 		Screen.repaint(); }
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		clickedPoint(arg0.getX(),arg0.getY());
-		Screen.Flag = false;
 		Screen.Pressed = false;
 		Screen.repaint(); } 
 	
@@ -117,19 +120,23 @@ class Panel extends JPanel {
 	public void mouseMoved(MouseEvent arg0) { } }
 	
 class ButtonAction implements ActionListener {
-	Panel Main;
+	MainPanel Panelext;
 	
-	ButtonAction(Panel panel) {
-		Main=panel;	}
-	
-	ButtonAction() {
-		super();
-	}
+	ButtonAction(MainPanel Panelext) {
+		this.Panelext=Panelext;	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		Main.clear();
+		Panelext.Flag = true;
+		Panelext.repaint();
 		
 	} }
 
+class Button extends JButton {
 	
+	Button() {
+	setLocation(0, 0);
+	setSize(80,20);
+	setText("CLEAN"); }
+
+}
